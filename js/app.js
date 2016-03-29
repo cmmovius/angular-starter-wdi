60623110f885892b7b829660f6d27926afd343a0
Christine Movius <christine.movius@gmail.com>
2016-03-29 09:32:38 -0400

    commented out more complex controller structure. superhero names and numbers show up.

--- js/app.js
+++ js/app.js
@@ -3,39 +3,44 @@
 (function(){
   angular
   .module("phonebook", [
-    "ui.router",
-    "heroes"
-  ])
-  .config([
-    "$stateProvider",
-    RouterFunction
-  ]);
+    // "ui.router"
+    // "heroes"
+  ]).controller("hero_controller", HeroController);
 
-  function RouterFunction($stateProvider){
-    $stateProvider
-    .state("heroIndex", {
-      url: "/heroes",
-      templateUrl: "js/heroes/index.html",
-      controller: "heroIndexController",
-      controllerAs: "heroIndexViewModel"
-    })
-    .state("heroNew", {
-      url: "/heroes/new",
-      templateUrl: "js/heroes/new.html",
-      controller: "heroNewController",
-      controllerAs: "heroNewViewModel"
-    })
-    .state("heroEdit", {
-      url: "/heroes/:id/edit",
-      templateUrl: "js/heroes/edit.html",
-      controller: "heroEditController",
-      controllerAs: "heroEditViewModel"
-    })
-    .state("heroShow", {
-      url: "/heroes/:id",
-      templateUrl: "js/heroes/show.html",
-      controller: "heroShowController",
-      controllerAs: "heroShowViewModel"
-    });
+  function HeroController(){
+    var vm = this;
+    vm.data = superheroList;
   }
+  // .config([
+  //   "$stateProvider",
+  //   RouterFunction
+  // ]);
+  //
+  // function RouterFunction($stateProvider){
+  //   $stateProvider
+  //   .state("heroIndex", {
+  //     url: "/heroes",
+  //     templateUrl: "js/heroes/index.html",
+  //     controller: "heroIndexController",
+  //     controllerAs: "heroIndexViewModel"
+  //   })
+  //   .state("heroNew", {
+  //     url: "/heroes/new",
+  //     templateUrl: "js/heroes/new.html",
+  //     controller: "heroNewController",
+  //     controllerAs: "heroNewViewModel"
+  //   })
+  //   .state("heroEdit", {
+  //     url: "/heroes/:id/edit",
+  //     templateUrl: "js/heroes/edit.html",
+  //     controller: "heroEditController",
+  //     controllerAs: "heroEditViewModel"
+  //   })
+  //   .state("heroShow", {
+  //     url: "/heroes/:id",
+  //     templateUrl: "js/heroes/show.html",
+  //     controller: "heroShowController",
+  //     controllerAs: "heroShowViewModel"
+  //   });
+  // }
 }());
